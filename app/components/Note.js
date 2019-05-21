d36277da74997fb36c71c8e8d094088911ea2295
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-21 17:13:07 -0500

    create note component

---
+++ app/components/Note.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
+
+class Note extends Component {
+  render() {
+    return (
+      <View>
+        <Text></Text>
+      </View>
+    )
+  }
+}
+
+export default Note;
