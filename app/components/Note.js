71a4ec107db8d677902f09ac862bc1f2b24f8c35
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-21 17:23:28 -0500

    style note component

--- app/components/Note.js
+++ app/components/Note.js
@@ -9,12 +9,40 @@ class Note extends Component {
         <Text style={styles.noteText}>{this.props.val.text}</Text>
 
         <TouchableOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
-
+          <Text style={styles.noteDeleteText}>D</Text>
         </TouchableOpacity>
 
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  note: {
+    position: 'absolute',
+    padding: 20,
+    paddingRight: 100,
+    borderBottomWidth: 2,
+    borderBottomColor: '#ededed',
+  },
+  noteText: {
+    paddingLeft: 20,
+    borderLeftWidth: 10,
+    borderLeftColor: '#e91e63',
+  },
+  noteDelete: {
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#2980b9',
+    padding: 10,
+    top: 10,
+    bottom: 10,
+    right: 10,
+  },
+  noteDeleteText: {
+    color: 'white'
+  }
+});
 
 export default Note;
