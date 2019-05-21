24ba168e9be1db41f2eaecfab65fa8d8968c59ad
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-21 14:11:44 -0500

    clean files

--- App.js
+++ App.js
@@ -1,16 +1,11 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import Main from './app/components/Main';
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-      </View>
+      <Main />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-  },
-});
